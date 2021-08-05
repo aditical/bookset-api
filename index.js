f963b5ace573cf49ad504d23dce18376272434a3
Aditi <aditi@gurzu.com>
2021-08-05 17:21:48 +0545

    created an api for the bookset

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+  res.end("It is working!!!");
+});
+
+app.get("/list_books", (req, res) => {
+  fs.readFile(__dirname + "/" + "bookset.json", "utf8", (err, data) => {
+    res.end(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
