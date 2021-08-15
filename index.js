d447bd832a898005bada2483dc5350ecca124f9d
Aditi <aditi@gurzu.com>
2021-08-15 21:09:02 +0545

    fixed cors error

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const fs = require("fs");
+const cors = require("cors");
 
 app.get("/", (req, res) => {
   res.end("It is working!!!");
 });
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET"],
+  })
+);
 
 app.get("/api/list_books", (req, res) => {
   fs.readFile(__dirname + "/" + "bookset.json", "utf8", (err, data) => {
