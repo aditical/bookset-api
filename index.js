5a447b3799516adf66467cce14eab0b93a6d737a
Aditi <aditi@gurzu.com>
2021-08-15 21:33:49 +0545

    cors addes

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const cors = require("cors");
 app.get("/", (req, res) => {
   res.end("It is working!!!");
 });
-
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET"],
-  })
-);
+app.use(cors());
 
 app.get("/api/list_books", (req, res) => {
   fs.readFile(__dirname + "/" + "bookset.json", "utf8", (err, data) => {
