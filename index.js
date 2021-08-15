79372f1c940de7e086889c35ef590010638d03c4
Aditi <aditi@gurzu.com>
2021-08-15 21:42:17 +0545

    fixes

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const cors = require("cors");
 app.get("/", (req, res) => {
   res.end("It is working!!!");
 });
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 app.get("/api/list_books", (req, res) => {
   fs.readFile(__dirname + "/" + "bookset.json", "utf8", (err, data) => {
