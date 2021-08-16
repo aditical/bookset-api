8bd346a56e4ab9833b9a9437fc3919fdbf79acb7
Aditi <aditi@gurzu.com>
2021-08-16 15:54:26 +0545

    cors fixes

--- index.js
+++ index.js
@@ -4,20 +4,10 @@ const port = 8000;
 const fs = require("fs");
 const cors = require("cors");
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.end("It is working!!!");
-});
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
 });
 
 app.get("/api/list_books", (req, res) => {
